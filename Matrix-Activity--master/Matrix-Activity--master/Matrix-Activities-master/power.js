a24b75627701c38f64550f7bee414cb4fa019470
DanyloKuleshko <7dkuleshko@stpatrick.org>
2017-03-13 10:32:24 -0500

    Add files via upload

---
+++ Matrix-Activity--master/Matrix-Activity--master/Matrix-Activities-master/power.js
@@ -0,0 +1,21 @@
+const Matrix = require("matrix-js");
+
+let A = Matrix([
+    [0,1,1,1],
+    [1,0,1,0],
+    [1,1,0,0],
+    [1,0,0,0]
+]);
+
+function power(mtrx,p){
+   let output = mtrx;
+    //mtrx is the matrix you input
+    //p is the power of the matrix you want
+    for(let x=0;x<p-1;x++){
+      output = Matrix(output.prod(A));
+    }
+    return output;
+}
+
+//testing code
+console.log(power(A,10));
